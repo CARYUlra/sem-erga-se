b9620638f881518e4bf894c65a314358a317e790
Your Name <CARYUlra>
2026-06-03 10:48:59 -0300

    banana

--- asimilação/script.js
+++ asimilação/script.js
@@ -1,10 +1,3 @@
-//Leia dois vetores de 5 elementos cada (valores inteiros). Crie um terceiro vetor que contenha apenas os elementos que estão em ambos os vetores (interseção, sem repetição). 
-//Exemplo: 
-//Vetor 1 → [1, 2, 3, 4, 5] 
-//Vetor 2 → [3, 4, 5, 6, 7] 
-//Resultado → [3, 4, 5] 
-
-// não use .push , .length , .string e outras funções de array, apenas atribuição direta e loops para manipular os vetores.
 let vetor1 = [];
 let vetor2 = [];
 let resultado = [];
